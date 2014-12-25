c5d57b8ba4e36fc429d2dc2ca5e943634c7970d9
rogerhokp <rogerhokp@gmail.com>
2014-12-25 22:55:38 +0800

    Remove non-necessary files Add press any key to exit

--- u115_bot.py
+++ u115_bot.py
@@ -4,7 +4,7 @@
 import time
 import logging
 import getopt
-
+import os
 import sys
 # import imp
 # imp.reload(sys)
@@ -15,74 +15,50 @@ from u115_api import u115_api
 arg_user = None
 arg_pass = None
 arg_sleeptime = 120
-arg_taskfile = None
+asg_folder = None
 
-# def readtask():
-#     global arg_taskfile
-#     return open(arg_taskfile, 'r', errors='ignore').readlines()
-
-# def update_task(context):
-#     #lock
-#     return open('task.txt', 'w').writelines(context)
-
-# def update_task_process(context):
-#     return open('taskprocess.txt', 'a').write(context + '\n')
-
+ 
 def update_task_success(context):
     return open('tasksuccess.txt', 'a').write(context + '\n')
 
 def update_task_error(context):
     return open('taskerror.txt', 'a').write(context + '\n')
 
+def get_torrent_files():
+    global asg_folder
+    return [f for f in os.listdir(asg_folder ) if (os.path.isfile(f) and f.find('.torrent') != -1) ] 
+
 def monitor():
     global arg_pass
     global arg_user
     global arg_sleeptime
-    global arg_taskfile
 
     u115 = u115_api()
     u115.login(arg_user, arg_pass)
-    ret = u115.add_torrent_task(arg_taskfile)
-    if ret : 
-        update_task_success('Torrent (%s) Added Success' % (arg_taskfile))
-    else :
-        update_task_error('Torrent (%s) Added Failed' % (arg_taskfile))
-    # while True:
-    #     try:
-    #         ret, result = u115.auto_make_share_link()
-    #         for res in result:
-    #             update_task_success('http://115.com/lb/%s ---- %s' % (res['Code'], res['Name']))
-    #         if 15 > u115.ret_current_bt_task_count():
-    #             task_list = readtask()
-    #             if len(task_list) == 0:
-    #                 logging.info('没有任务文件...zZZ')
-    #             else:
-    #                 i = task_list[0]
-    #                 task_list.pop(0)
-    #                 logging.info('adding torrent : %s ' % i )
-    #                 update_task(task_list)
-    #                 if not i.startswith("http"):
-    #                     continue
-    #                 update_task_process(i)
-    #                 u115.add_http_task(i)
-    #                 continue
-    #         time.sleep(arg_sleeptime)
-    #     except KeyboardInterrupt:
-    #         break
-    #     except:
-    #         import traceback
-    #         print(traceback.print_exc())
-    #         time.sleep(arg_sleeptime * 10)
-    #         u115 = u115_api()
-    #         u115.login(arg_user, arg_pass)
+    torrents = get_torrent_files();
 
+
+    logging.info('find Number of torrent %s in %s' % (len(torrents) , asg_folder) )
+
+    for torrent in torrents:
+        logging.info('\n************** adding torrent %s **************\n' % torrent)
+        ret = u115.add_torrent_task(torrent)
+        if ret : 
+            os.remove(torrent)
+            update_task_success('Torrent (%s) Added Success' % (torrent))
+        else :
+            update_task_error('Torrent (%s) Added Failed' % (torrent))
+
+    input("Press Enter to continue...")
+ 
 def Usage():
     print('115Bot usage:')
     print('-u, --user: 115 account login user')
     print('-p, --pass: 115 account login pass')
-    print('-f, --file: task list file\n')
+    print('-f, --folder: torrent folder path\n')
     print('-h, --help: print help message.')
     print('-v, --version: print script version')
+
 
 def Version():
     print('115Bot ver 0.1')
@@ -91,7 +67,7 @@ def main(argv):
     global arg_pass
     global arg_user
     global arg_sleeptime
-    global arg_taskfile
+    global asg_folder
 
     try:
         opts, args = getopt.getopt(argv[1:], 'u:p:f:v:h', ['user=', 'pass=', 'file=', 'help', 'version'])
@@ -107,25 +83,24 @@ def main(argv):
         elif o in ('-v', '--version'):
             Version()
             sys.exit(0)
-        elif o in ('-f', '--file'):
-            arg_taskfile = a
         elif o in ('-u', '--user'):
             arg_user = a
         elif o in ('-p', '--pass'):
             arg_pass = a
+        elif o in ('-f', '--folder'):
+            asg_folder = a
         else:
             print('unhandled option')
             sys.exit(1)
 
-    if arg_taskfile is None:
-        print('missing arg -f')
-        sys.exit(2)
     if arg_user is None:
         print('missing arg -u')
         sys.exit(2)
     if arg_pass is None:
         print('missing arg -p')
         sys.exit(2)
+
+    asg_folder = asg_folder or '.'
     monitor()
 
 if __name__ == '__main__':
